92457a7036e71d8af5e6d1a9858fa15a4ec6f4a7
juanrodrigues <juanrodriguessantos95@gmail.com>
2022-04-05 09:56:15 -0300

    Aula 56 filtrando catetoria por lanche

--- Controllers/LancheController.cs
+++ Controllers/LancheController.cs
@@ -1,4 +1,5 @@
-﻿using LanchesMac.Repositories.Interfaces;
+﻿using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
 using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,14 +13,37 @@ namespace LanchesMac.Controllers
             _lancheRepository = lancheRepository;
         }
 
-        public IActionResult List()
+        public IActionResult List(string Categoria)
         {
-            //var lanches = _lancheRepository.Lanches;
-            //return View(lanches);
-            var lanchesListViewModel = new LancheListViewModel();
-            lanchesListViewModel.Lanches = _lancheRepository.Lanches;
-            lanchesListViewModel.CategoriaAtual = "Categoria Atual";
+            IEnumerable<Lanche> lanches;
+            string categoriaAtual =string.Empty;
+    
+            if (string.IsNullOrEmpty(Categoria)){
+                lanches = _lancheRepository.Lanches.OrderBy(l => l.LancheId);
+                categoriaAtual = "Todos os Lanches";
+            }
+            else
+            {
+                if (string.Equals("Normal", Categoria, StringComparison.OrdinalIgnoreCase))
+                {
+                    lanches = _lancheRepository.Lanches
+                    .Where(x => x.Categoria.CategoriaNome.Equals("Normal")).OrderBy(x => x.Nome);
 
+                }
+                else
+                {
+                    lanches = _lancheRepository.Lanches
+                        .Where(l => l.Categoria.CategoriaNome.Equals("Natural")).OrderBy(x => x.Nome);
+                }
+                categoriaAtual = Categoria.ToUpper();
+               
+            
+            }
+            var lanchesListViewModel = new LancheListViewModel
+            {
+                Lanches = lanches,
+                CategoriaAtual = categoriaAtual
+            };
             return View(lanchesListViewModel);
         }
     }
